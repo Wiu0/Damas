860ef0fc993d48f5508f4180162372c044739bda
Ti103 <54046502+Ti103@users.noreply.github.com>
2019-10-25 23:17:56 -0300

    Add files via upload

---
+++ GreenFoot/GreenFoot/BluePiece.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BluePiece here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BluePiece extends Peca
+{
+    /**
+     * Act - do whatever the BluePiece wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        casasDisponiveis();
+        tiraCasa();
+    }
+}
